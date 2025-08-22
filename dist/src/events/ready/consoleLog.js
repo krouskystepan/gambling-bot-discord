240b82685a328de23ea52ae1fea1b12f7b825d41
Štěpán Krouský <stepan.krousky@seznam.cz>
2025-08-22 17:44:47 +0200

    update dependencies

--- dist/src/events/ready/consoleLog.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = async (client) => {
-    const currentTime = new Date().toLocaleString('cs-CZ');
-    console.log(`✅ ${client.user?.tag} is online\n🕛 Time: ${currentTime}`);
-};
