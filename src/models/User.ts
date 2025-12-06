0fd05949e51ae0419abf37b79a32566e94c1db03
Štěpán Krouský <stepan.krousky@seznam.cz>
2025-12-06 22:05:10 +0100

    fix warnings

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,4 @@
 import { TUser, UserSchema } from 'gambling-bot-shared'
 import { model } from 'mongoose'
 
-UserSchema.index({ userId: 1, guildId: 1 }, { unique: true })
-
 export default model<TUser>('User', UserSchema)
--- src/models/VipRoom.ts
+++ src/models/VipRoom.ts
@@ -1,7 +1,4 @@
 import { VipRoomSchema, TVipRoom } from 'gambling-bot-shared'
 import { model } from 'mongoose'
 
-VipRoomSchema.index({ expiresAt: 1 })
-VipRoomSchema.index({ userId: 1, guildId: 1 }, { unique: true })
-
 export default model<TVipRoom>('VipRoom', VipRoomSchema)
