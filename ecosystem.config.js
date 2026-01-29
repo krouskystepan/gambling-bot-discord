c41dc2176ef8eb763b625eb9a4eedf54788bf197
Štěpán Krouský <stepan.krousky@seznam.cz>
2026-01-29 22:37:54 +0100

    add new deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'gambling-bot',
+      script: 'src/index.ts',
+      interpreter: 'node',
+      interpreter_args: '--loader tsx',
+      cwd: '/var/www/apps/gambling-bot-discord',
+
+      exec_mode: 'fork',
+      instances: 1,
+
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '300M',
+
+      env: {
+        NODE_ENV: 'production'
+      },
+      env_file: '/var/www/envs/gambling-bot.env'
+    }
+  ]
+}
